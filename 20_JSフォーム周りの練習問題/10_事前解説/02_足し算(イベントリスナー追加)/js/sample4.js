15653cc20dfaf430ef881f54b33f1a9bdd8bfd83
119003 <it.da.fukase@iftc.co.jp>
2022-08-01 11:43:52 +0900

    アロー関数に修正

--- 20_JSフォーム周りの練習問題/10_事前解説/02_足し算(イベントリスナー追加)/js/sample4.js
+++ 20_JSフォーム周りの練習問題/10_事前解説/02_足し算(イベントリスナー追加)/js/sample4.js
@@ -1,14 +1,14 @@
 const calc = () => {
-	const leftNumDom = document.getElementById("left-number");
-	const rightNumDom = document.getElementById("right-number");
-	const answerNum = Number(leftNumDom.value) + Number(rightNumDom.value);
-	const resultDom = document.getElementById("answer");
-	resultDom.innerText = answerNum;
+  const leftNumDom = document.getElementById("left-number");
+  const rightNumDom = document.getElementById("right-number");
+  const answerNum = Number(leftNumDom.value) + Number(rightNumDom.value);
+  const resultDom = document.getElementById("answer");
+  resultDom.innerText = answerNum;
 };
 
 // 画面描画後に実行
-window.onload = function () {
-	// ボタンのDOM取得
-	const btnDom = document.querySelector("button");
-	btnDom.addEventListener("click", calc);
+window.onload = () => {
+  // ボタンのDOM取得
+  const btnDom = document.querySelector("button");
+  btnDom.addEventListener("click", calc);
 };
