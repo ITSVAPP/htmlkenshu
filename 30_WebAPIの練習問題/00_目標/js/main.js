55c92c8d73778df1e560732235aca2ecc247c15d
ITSVAPP <it.da.fukase@iftc.co.jp>
2023-07-27 13:08:29 +0900

    回答のコメントアウト追加

--- 30_WebAPIの練習問題/00_目標/js/main.js
+++ 30_WebAPIの練習問題/00_目標/js/main.js
@@ -1,20 +1,27 @@
+// Pokemonを検索する非同期関数
 const searchPokemon = async (no) => {
   try {
+    // APIから特定の番号のPokemon情報を取得
     const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + no);
     if (!res.ok) {
+      // レスポンスが成功していなければエラーを投げる
       throw new Error(res.statusText);
     }
     const pokemon = await res.json();
+    // 取得したPokemon情報を表示
     viewPokemon(pokemon);
   } catch (e) {
+    // エラー時はロードアイコンを非表示にし、エラーメッセージを表示
     document.querySelector(".spinner").classList.add("hidden");
     document.querySelector(".err-message").classList.remove("hidden");
   }
 };
 
+// 取得したPokemon情報を画面に表示する非同期関数
 const viewPokemon = async (pokemon) => {
   const { id, name, height, weight, types } = pokemon;
   const { front_default, back_default } = pokemon.sprites;
+  // Pokemonの各種情報を表示
   document.getElementById("no").textContent = ("00000" + id).slice(-5);
   document.getElementById("name").textContent = name;
   document.getElementById("height").textContent =
@@ -25,29 +32,34 @@ const viewPokemon = async (pokemon) => {
   document.getElementById("front_default").src = front_default;
 
   if (back_default) {
+    // 背面画像がある場合は表示
     document.getElementById("back_default").classList.remove("hidden");
     document.getElementById("back_default").src = back_default;
   } else {
+    // 背面画像がない場合は非表示
     document.getElementById("back_default").classList.add("hidden");
   }
 
-  //ロード完了時に表示
+  // 画像のロードが完了した時点で表示
   document.getElementById("front_default").addEventListener("load", () => {
     view();
   });
 };
 
+// 検索開始前の初期表示処理
 const load = () => {
   document.querySelector(".box-result").classList.add("invisible");
   document.querySelector(".spinner").classList.remove("hidden");
   document.querySelector(".err-message").classList.add("hidden");
 };
 
+// 検索結果の表示処理
 const view = () => {
   document.querySelector(".spinner").classList.add("hidden");
   document.querySelector(".box-result").classList.remove("invisible");
 };
 
+// ボタンクリック時の処理
 const btnClick = async () => {
   const no = document.getElementById("input-text").value;
   if (no) {
@@ -56,13 +68,29 @@ const btnClick = async () => {
   }
 };
 
+// ページ読み込み時の処理
 window.onload = async () => {
   load();
+  // 初期表示でピカチュウ（No.25）を表示
   await searchPokemon(25);
+  // クリックとEnterキーにイベントをバインド
   document.getElementById("btn").addEventListener("click", btnClick);
   document.getElementById("input-text").addEventListener("keyup", (e) => {
     if (e.key === "Enter") {
       btnClick();
     }
   });
+};
+
+// 以下はカンニング防止用のコード
+// 右クリック禁止
+document.oncontextmenu = function () {
+  return false;
+};
+
+// 開発者コンソールの表示禁止
+window.document.onkeydown = function (event) {
+  if (event.key == "F12") {
+    return false;
+  }
 };
