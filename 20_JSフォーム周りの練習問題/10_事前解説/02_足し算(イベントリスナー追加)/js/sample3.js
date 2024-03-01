fa7bb15e68f2a6841266a2d8ae3d139a6be6665f
ITSVAPP <it.da.fukase@iftc.co.jp>
2024-03-01 16:26:37 +0900

    フォーマッター微修正

--- 20_JSフォーム周りの練習問題/10_事前解説/02_足し算(イベントリスナー追加)/js/sample3.js
+++ 20_JSフォーム周りの練習問題/10_事前解説/02_足し算(イベントリスナー追加)/js/sample3.js
@@ -1,13 +1,13 @@
 // 画面描画後に実行
 window.onload = () => {
-    // ボタンのDOM取得
-    const btnDom = document.querySelector("button");
-    btnDom.addEventListener("click", () => {
-        const leftNumDom = document.getElementById("left-number");
-        const rightNumDom = document.getElementById("right-number");
-        const answerNum = Number(leftNumDom.value) + Number(rightNumDom.value);
+  // ボタンのDOM取得
+  const btnDom = document.querySelector("button");
+  btnDom.addEventListener("click", () => {
+    const leftNumDom = document.getElementById("left-number");
+    const rightNumDom = document.getElementById("right-number");
+    const answerNum = Number(leftNumDom.value) + Number(rightNumDom.value);
 
-        const resultDom = document.getElementById("answer");
-        resultDom.innerText = answerNum;
-    });
+    const resultDom = document.getElementById("answer");
+    resultDom.innerText = answerNum;
+  });
 };
